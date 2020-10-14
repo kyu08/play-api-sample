ee0dcab03867876946852d3888e70b45c226c780
kyu08 <kyu08@users.noreply.github.com>
2020-10-14 16:00:44 +0900

    implement POST method

--- test/controllers/HelloControllerSpec.scala
+++ test/controllers/HelloControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.libs.json.Json
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -9,12 +10,28 @@ class HelloControllerSpec extends PlaySpec with GuiceOneAppPerTest {
 
   "HelloController GET" must {
 
-    "/hello にGETメソッドでアクセスできる" in {
+    "/hello にGETメソッドでアクセスすると hello world が返る " in {
       val request = FakeRequest(GET, "/hello")
       val response = route(app, request).get
 
       status(response) mustBe OK
+      contentType(response) mustBe Some("text/plain")
+      contentAsString(response) mustBe "Hello World!"
     }
   }
 
+  "HelloController POST" must {
+
+    "Hello に POST メソッドでアクセスするとJSONがかえる" in {
+      val request = FakeRequest(POST, "/hello")
+      val response = route(app, request).get
+
+      status(response) mustBe OK
+      contentType(response) mustBe Some("application/json")
+      contentAsJson(response) mustBe Json.obj(
+        "hello" -> "world",
+        "language" -> "scala"
+      )
+    }
+  }
 }
